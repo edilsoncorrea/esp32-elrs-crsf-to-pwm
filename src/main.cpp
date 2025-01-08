717467118bc3b7b9b44dcf5e166ed4fe1402ed53
edilsoncorrea <edilsoncorrea117@gmail.com>
2025-01-08 09:21:28 -0300

    Criação do branch esp32c3 com ajustes de pinos e a placa para dfrobot_beetle_esp32c3.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,28 @@
 #include <Arduino.h>
 #include "crsf.h"
 
-#define RXD2 16
-#define TXD2 17
+#define RXD2 6
+#define TXD2 7
+
 #define SBUS_BUFFER_SIZE 25
 uint8_t _rcs_buf[25] {};
 uint16_t _raw_rc_values[RC_INPUT_MAX_CHANNELS] {};
 uint16_t _raw_rc_count{};
 
-int aileronsPin = 12;
-int elevatorPin = 13;
-int throttlePin = 14;
-int rudderPin = 15;
+int aileronsPin = 1;
+int elevatorPin = 9;
+int throttlePin = 3;
+int rudderPin = 10;
 
 int aileronsPWMChannel = 1;
 int elevatorPWMChannel = 2;
 int throttlePWMChannel = 3;
 int rudderPWMChannel = 4;
 
+#define IN1 2
+#define IN2 3
+#define IN3 4
+#define IN4 5
 
 void SetServoPos(float percent, int pwmChannel)
 {
@@ -31,31 +36,42 @@ void SetServoPos(float percent, int pwmChannel)
     uint32_t duty = map(percent, 0, 100, 3276.8, 6553.6);
 
     ledcWrite(pwmChannel, duty);
+
 }
 
 void setup() {
   // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
-  Serial.begin(460800);
-  //Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial2.begin(420000, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("Serial Txd is on pin: "+String(TX));
-  Serial.println("Serial Rxd is on pin: "+String(RX));
+  Serial.begin(115200);
+
+  Serial.println("Setup");
+
+  Serial1.begin(420000, SERIAL_8N1, RXD2, TXD2);
+
+  Serial.println("Serial Txd is on pin: " + String(TX));
+  Serial.println("Serial Rxd is on pin: " + String(RX));
   
-  ledcSetup(aileronsPWMChannel,50,16);
-  ledcSetup(elevatorPWMChannel,50,16);
-  ledcSetup(throttlePWMChannel,50,16);
-  ledcSetup(rudderPWMChannel,50,16);
+  ledcSetup(aileronsPWMChannel, 50, 16);
+  ledcSetup(elevatorPWMChannel, 50, 16);
+  ledcSetup(rudderPWMChannel, 50, 16);
+
+  ledcSetup(throttlePWMChannel, 50, 16);
 
   ledcAttachPin(aileronsPin, aileronsPWMChannel);
   ledcAttachPin(elevatorPin, elevatorPWMChannel);
-  ledcAttachPin(throttlePin, throttlePWMChannel);
   ledcAttachPin(rudderPin, rudderPWMChannel);
+
+  ledcAttachPin(throttlePin, throttlePWMChannel);
 }
 
 void loop() { //Choose Serial1 or Serial2 as required
-  // Serial.println("looping");
-  while (Serial2.available()) {
-    size_t numBytesRead = Serial2.readBytes(_rcs_buf, SBUS_BUFFER_SIZE);
+   Serial.println("looping");
+  
+  while (Serial1.available()) {
+    size_t numBytesRead = Serial1.readBytes(_rcs_buf, SBUS_BUFFER_SIZE);
+    
+    Serial.print("bytes lidos");
+    Serial.println(numBytesRead);
+
     if(numBytesRead > 0)
     {
       crsf_parse(&_rcs_buf[0], SBUS_BUFFER_SIZE, &_raw_rc_values[0], &_raw_rc_count, RC_INPUT_MAX_CHANNELS );
@@ -72,14 +88,14 @@ void loop() { //Choose Serial1 or Serial2 as required
 
       int aileronsMapped = map(_raw_rc_values[0], 1000, 2000, 0, 100);
       int elevatorMapped = map(_raw_rc_values[1], 1000, 2000, 0, 100);
-      int throttleMapped = map(_raw_rc_values[2], 1000, 2000, 0, 100);
       int rudderMapped = map(_raw_rc_values[3], 1000, 2000, 0, 100);
       int switchMapped = map(_raw_rc_values[4], 1000, 2000, 0, 100);
+      int throttleMapped = map(_raw_rc_values[2], 1000, 2000, 0, 100);
 
       SetServoPos(aileronsMapped, aileronsPWMChannel);
       SetServoPos(elevatorMapped, elevatorPWMChannel);
-      SetServoPos(throttleMapped, throttlePWMChannel);
       SetServoPos(rudderMapped, rudderPWMChannel);
+      SetServoPos(throttleMapped, throttlePWMChannel);
     }
   }
 }
